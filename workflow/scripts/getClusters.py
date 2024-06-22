ecf152661e4d7eedd8fcc09bf722a13e06097217
TheSkyentist <raphael.hviding@gmail.com>
2024-06-22 12:47:03 +0100

    Proper Spherical Geometry for Pairing Regions Together

--- workflow/scripts/getClusters.py
+++ workflow/scripts/getClusters.py
@@ -1,20 +1,88 @@
 #! /usr/bin/env python
 
-# Import packages
+# Python Packages
 import os
 import json
 import glob
 import shutil
 import argparse
-import numpy as np
 from tqdm import tqdm
-from sregion import SRegion
-from astropy.io import fits
-from shapely import union_all
+from multiprocessing import Pool
 import xml.etree.ElementTree as et
+
+# Computational Packages
+import numpy as np
+from scipy.optimize import minimize
+
+# Astropy Packages
+from astropy.io import fits
+
 from astroquery.mast import Observations
 from astropy.table import Table,join,vstack
 from astropy.coordinates import SkyCoord,get_constellation
+
+# Geometry packages
+from sregion import SRegion
+from spherical_geometry import great_circle_arc
+from spherical_geometry.polygon import SingleSphericalPolygon
+
+# Parametric equation for an arc from 0 to 1
+def parametric_arc(a,b):
+
+    # Get angle between
+    theta = np.arccos(np.dot(a,b))
+
+    # Get perpendicular vector
+    c = np.cross(np.cross(a,b),a)
+    c /= np.sqrt(c.dot(c)) # Normalize to unit
+
+    # Define arc function
+    def arc(t):
+        scaled_t = t*theta
+        return a*np.cos(scaled_t) + c*np.sin(scaled_t)
+    return arc
+
+# Distance between two arcs
+def distance_between_arcs(a,b,c,d):
+
+    # If points are the same, skip, essentially
+    if np.dot(a,b) > 1 or np.dot(c,d) > 1: return np.inf
+
+    # Get arcs from points
+    arc1 = parametric_arc(a,b)
+    arc2 = parametric_arc(c,d)
+    
+    # Minimize distance between arcs
+    fun = lambda t: np.arccos(np.dot(arc1(t[0]),arc2(t[1])))
+    return minimize(fun,[0.5,0.5],bounds=([0,1],[0,1])).fun
+
+# Get distances between Shapely Objects (in Degrees)
+# But done properly in spherical coordinates
+# Don't check above some minimum distance, as it is not worth it
+def distance_between_shapely(sA,sB,min_dist=1):
+
+    # Check if MultiPolygon (can only be sA)
+    if hasattr(sA,'geoms'): return np.min([distance_between_shapely(g,sB) for g in sA.geoms]).min()*180/np.pi
+
+    # Get SingleSphericalPolygons
+    spA = SingleSphericalPolygon.from_radec(*sA.exterior.xy)
+    spB = SingleSphericalPolygon.from_radec(*sB.exterior.xy)
+
+    # Get points from spherical polygons
+    spA_points = spA.points
+    spB_points = spB.points
+
+    # Don't bother checking if we are further apart than ~1 degree
+    if np.arccos(np.dot(spA_points[0],spB_points[0]))*180/np.pi > min_dist: return np.inf
+
+    # Get distances between arcs
+    return np.min(
+        [distance_between_arcs(*i) for i in 
+            [
+                (spA_points[i],spA_points[i+1],spB_points[j],spB_points[j+1])
+                    for i in range(len(spA_points)-1) for j in range(len(spB_points)-1)
+                ]
+        ])*180/np.pi
 
 if __name__ == '__main__':
 
@@ -27,6 +95,7 @@ if __name__ == '__main__':
     proposal_ids = ['1571','3383','4681']
 
     # Query survey
+    print('Querying MAST...')
     obs = Observations.query_criteria(
         proposal_id=proposal_ids,
         instrument_name="NIRISS*",
@@ -34,6 +103,7 @@ if __name__ == '__main__':
     obs.sort('t_obs_release') # Sort by release date
 
     # Download APT files
+    print('Downloading APT files...')
     if not os.path.isdir('APT'): os.mkdir('APT')
     for i in proposal_ids:
         os.system(f'wget -q --show-progress https://www.stsci.edu/jwst/phase2-public/{i}.aptx')
@@ -73,6 +143,7 @@ if __name__ == '__main__':
     obs.add_column(pids,name='prim_id')
 
     # Compute regions
+    print('Computing Overlapping Regions (Cartesian Approximation)...')
     regs = [SRegion(o).shapely[0] for o in obs['s_region']]
     obs = [Table(o) for o in obs]
 
@@ -88,8 +159,8 @@ if __name__ == '__main__':
         # Iterate over regions
         for i,r in enumerate(regs):
 
-            # Check if region intersects with most recent cluster
-            if c_regs[-1].intersects(r) and c_regs[-1].distance(r) < 30/3600:
+            # Check if region intersects with most recent cluster (approximating in Cartesian)
+            if c_regs[-1].intersects(r):
 
                 # Combine regions and observations
                 c_regs[-1] = c_regs[-1].union(regs.pop(i))
@@ -107,8 +178,31 @@ if __name__ == '__main__':
             c_regs.append(regs.pop(0))
             clusters.append(obs.pop(0))
 
-    # Save clusters
-    jmax = 0
+    # Combine clusters that are too close
+    print('Combine Nearly Clusters (Spherical)...')
+    
+    # Check distance between clusters (in Spherical)
+    i = 0
+    while i < len(clusters):
+
+        # Check against all other clusters
+        for j in range(i+1,len(clusters)):
+
+            # Check distance between regions
+            if distance_between_shapely(c_regs[i],c_regs[j]) < 30/3600:
+
+                # Combine clusters
+                c_regs[i] = c_regs[i].union(c_regs.pop(j))
+                clusters[i] = vstack([clusters[i],clusters.pop(j)])
+
+                # Break loop and start over
+                i -= 1 # Decrement index to counteract increment
+                break
+
+        # Increment index
+        i += 1
+
+    # Get cluster names from Constellations
     names = []
     for i,c in enumerate(clusters):
 
@@ -120,7 +214,6 @@ if __name__ == '__main__':
         while f'{con}-{str(j).zfill(2)}' in names:
             j += 1
         name = f'{con}-{str(j).zfill(2)}'
-        if j > jmax: jmax = j
 
         # Keep track
         names.append(name)
@@ -129,6 +222,7 @@ if __name__ == '__main__':
     with open('CLUSTERS/clusters.txt','w') as f: f.write('\n'.join(names))
 
     # Save clusters to FITS file
+    print('Querying Products and Saving to FITS...')
     obs_cols = ['obs_id','s_ra','s_dec','filters','t_obs_release','t_exptime','obsid','s_region','prim_id'] # Columns to keep
     prod_cols = ['obs_id','productFilename','dataURI','obs_collection','obsID']
     obs_hdul = fits.HDUList([fits.PrimaryHDU()])
@@ -143,7 +237,7 @@ if __name__ == '__main__':
         allprods = Observations.get_product_list(c)
         good = np.logical_and(
             allprods['productType']=='SCIENCE',
-            allprods['productSubGroupDescription']=='UNCAL'
+            allprods['productSubGroupDescription']=='UNCAL' # Uncal files only
             )
         prods = allprods[good]
 
@@ -158,6 +252,8 @@ if __name__ == '__main__':
 
     # Skip if download flag is not set
     if args.download:
+
+        print('Downloading Products...')
 
         # Get products to download
         prods = vstack([Table(p.data) for p in prod_hdul[1:]])
