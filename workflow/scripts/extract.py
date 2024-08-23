95a999eeafdec7faf2c6d6bde9d2a27cd9012dd1
TheSkyentist <raphael.hviding@gmail.com>
2024-08-23 15:37:00 +0200

    Clean up

--- workflow/scripts/extract.py
+++ workflow/scripts/extract.py
@@ -114,7 +114,7 @@ def main():
     # Determine catalog depth
     cat = Table.read(os.path.join(extract, f'{fname}-ir.cat.fits'))
     mag = cat['MAG_AUTO'][np.invert(cat['MAG_AUTO'].mask)]
-    kde = stats.gaussian_kde(mag[np.invert(mag.mask)])  # KDE Estimate
+    kde = stats.gaussian_kde(mag)  # KDE Estimate
     mode_mag = optimize.minimize(lambda x: -kde(x), np.median(mag)).x  # Modes
 
     # Determine exposure time offset
