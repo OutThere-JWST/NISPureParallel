defaac46abbbd563cc3740c68b88571b4c5c2af5
TheSkyentist <raphael.hviding@gmail.com>
2025-08-14 15:18:15 +0200

    No CRDS

--- workflow/scripts/contamination.py
+++ workflow/scripts/contamination.py
@@ -66,7 +66,7 @@ def model_contam(fname, filt, directs, dirs, mags, projs, grism_files):
         files=grism_files,
         model_kwargs={'compute_size': False, 'size': 48},
         subtract_median_filter=False,
-        use_jwst_crds=True,
+        use_jwst_crds=False,
         force_ref=ref,
         cpu_count=1,
         refine_threads=1,
