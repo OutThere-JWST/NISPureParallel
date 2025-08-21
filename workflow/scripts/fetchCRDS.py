4ab714ba05823d9b8914924ad8a243c98f6731e1
TheSkyentist <raphael.hviding@gmail.com>
2025-08-21 11:26:44 +0200

    Minimal NIRISS download

---
+++ workflow/scripts/fetchCRDS.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+from tqdm import tqdm
+from astropy.io import fits
+from astropy.table import vstack, Table
+from crds import getreferences
+
+# Concatenate all files
+fields = vstack([Table(h.data) for h in fits.open('FIELDS/fields.fits')[1:]])
+
+# Assign correct names to columns
+fields['date-obs'] = [i.split('T')[0] for i in fields['date_obs']]
+fields['time-obs'] = [i.split('T')[1] for i in fields['date_obs']]
+fields['pupil'] = fields['niriss_pupil']
+
+# Necessary keys for CRDS
+keys = [
+    'date-obs',
+    'time-obs',
+    'instrume',
+    'detector',
+    'filter',
+    'pupil',
+    'exp_type',
+    'readpatt',
+    'subarray',
+]
+for f in tqdm(fields):
+    getreferences({c.upper(): f[c] for c in keys})
